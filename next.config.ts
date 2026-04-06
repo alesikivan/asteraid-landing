a7703438bda38090a50b69ec6a29218a2e290584
alesikivan <obcofficiall@gmail.com>
2026-04-06 23:35:09 +0300

    upd next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createNextIntlPlugin from 'next-intl/plugin'
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts')
 
+
 // @ts-ignore
 const isProd = process.env.NODE_ENV === 'production'
 
